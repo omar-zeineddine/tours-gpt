9ed73cd2630e8de8a594df8adbb217c07eef40b9
Omar Zeineddine <ozeineddine.dev@gmail.com>
2024-01-24 10:54:39 +0200

    feat - complete chat page

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,26 +1,53 @@
 'use client';
+
 import React from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { generateChatResponse } from '@/utils/actions';
+import { toast } from 'sonner';
 
 const Chat = () => {
   const [text, setText] = React.useState('');
-  const [messages, setMessages] = React.useState([]);
+  const [messages, setMessages] = React.useState<any[]>([]);
 
-  const { mutate } = useMutation({
-    mutationFn: (message) => generateChatResponse(message),
+  const { mutate, isPending } = useMutation({
+    mutationFn: (query) => generateChatResponse([...messages, query]),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error('Something went wrong');
+        return;
+      }
+      setMessages((prev) => [...prev, data]);
+    },
   });
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     // console.log(text);
-    mutate(text);
+    const query = { role: 'user', content: text };
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setText('');
   };
+
+  // console.log(messages);
 
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div>
-        <h2 className="text-2xl">Messages</h2>
+        {messages.map(({ role, content }, index) => {
+          const avatar = role == 'user' ? '👤' : '🤖';
+          const bgColor = role == 'user' ? 'bg-base-200' : 'bg-base-100';
+          return (
+            <div
+              key={index}
+              className={`${bgColor} flex py-6 -mx-8 px-8 text-md leading-loose border-b border-base-300`}
+            >
+              <span className="mr-4">{avatar}</span>
+              <p className="max-w-7xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending ? <span className="loading mt-2"></span> : null}
       </div>
       <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
         <div className="join w-full">
@@ -32,8 +59,12 @@ const Chat = () => {
             required
             onChange={(e) => setText(e.target.value)}
           />
-          <button className="btn btn-primary join-item" type="submit">
-            Submit
+          <button
+            className="btn btn-primary join-item"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending ? 'please wait...' : 'Send'}
           </button>
         </div>
       </form>
--- utils/actions.ts
+++ utils/actions.ts
@@ -3,17 +3,21 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI();
 
-export const generateChatResponse = async (chatMessage: any) => {
-  // console.log(chatMessage);
-  const response = await openai.chat.completions.create({
-    messages: [
-      { role: 'system', content: 'you are a helpful assistant' },
-      { role: 'user', content: chatMessage },
-    ],
-    model: 'gpt-3.5-turbo',
-    temperature: 0.1,
-  });
-  console.log(response.choices[0].message);
-  console.log(response);
-  return 'awesome';
+export const generateChatResponse = async (chatMessages: any) => {
+  try {
+    // console.log(chatMessage);
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: 'you are a helpful assistant' },
+        ...chatMessages,
+      ],
+      model: 'gpt-3.5-turbo',
+      temperature: 0.1,
+    });
+    // console.log(response.choices[0].message);
+    // console.log(response);
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 };
